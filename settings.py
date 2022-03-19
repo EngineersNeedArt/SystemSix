10f2332b5c11dc4a585dd9fe9dbc258d303cf0ba
EngineersNeedArt <84056606+EngineersNeedArt@users.noreply.github.com>
2022-03-19 10:15:04 -0500

    Initial commit.

---
+++ settings.py
@@ -0,0 +1,12 @@
+# Edit this file to contain your personal settings. Below are just placeholder values.
+LOCALE = "en_US.UTF-8"
+
+LATITUDE = 41.2853
+LONGITUDE = -96.2050
+
+# Monday = 1, ... Sunday = 7; None = never show full; other value = random.
+TRASH_DAY = 1
+
+# See README.md on instructions to assign your WebDAV Calendar URL.
+WEBDAV_CALENDAR_URL = None
+WEBDAV_IS_APPLE = True
